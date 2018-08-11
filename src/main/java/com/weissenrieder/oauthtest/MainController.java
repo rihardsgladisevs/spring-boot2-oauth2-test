3455a3f5e5c8ccdbb9ba4074385e95e96d832e91
rg <rg@weissenrieder.com>
2018-08-11 16:19:42 +0300

    additional information on user info page

--- src/main/java/com/weissenrieder/oauthtest/MainController.java
+++ src/main/java/com/weissenrieder/oauthtest/MainController.java
@@ -3,9 +3,11 @@ package com.weissenrieder.oauthtest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
@@ -62,6 +65,11 @@ public class MainController {
                     .block();
         }
         model.addAttribute("userAttributes", userAttributes);
+        OAuth2User principal = authentication.getPrincipal();
+        Map<String, Object> authenticationAttributes = principal.getAttributes();
+        model.addAttribute("authenticationAttributes", authenticationAttributes);
+        Collection<? extends GrantedAuthority> authorities = principal.getAuthorities();
+        model.addAttribute("grantedAuthorities", authorities);
         return "userinfo";
     }
 
